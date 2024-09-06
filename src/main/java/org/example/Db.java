6fd3c27760f5725425e604f0f75f3e23d740f46f
TeflonDervish <voltvilty45@gmail.com>
2024-09-06 18:20:47 +0500

    Init commit

---
+++ src/main/java/org/example/Db.java
@@ -0,0 +1,70 @@
+package org.example;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.List;
+
+public class Db {
+
+    private static Connector connector;
+
+
+    public static void connetcDb() {
+
+        connector = new Connector();
+
+    }
+
+    public static void addPerson(Person person) {
+
+        try (Session session = connector.getSession();) {
+            session.beginTransaction();
+            session.save(person);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+
+        }
+
+    }
+
+    public static void addAllPerson(List<Person> personList) {
+
+        try (Session session = connector.getSession()) {
+            session.beginTransaction();
+            personList.stream().map(x -> session.save(x));
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public static List<Person> getAllPerson() {
+
+        try (Session session = connector.getSession()) {
+
+            return session.createQuery("FROM Person", Person.class).getResultList();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void deleteAll() {
+        try (Session session = connector.getSession()) {
+            Transaction t = session.beginTransaction();
+            List<Person> books = session.createQuery("FROM Person",
+                    Person.class).getResultList();
+            books.forEach(b -> {
+                session.delete(b);
+            });
+            t.commit();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+}
---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,41 @@
+package org.example;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "test.person")
+public class Person {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "age")
+    private int age;
+
+    public Person(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public Person() {}
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
